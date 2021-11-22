f375a02195eff5f6460352654c338f9b349882a8
Mark Melczer <melczer7@gmail.com>
2021-11-22 21:35:23 +0100

    Added json deserialization parser.

---
+++ utils.ts
@@ -0,0 +1,19 @@
+export function parseInstruction(jsonString) {
+    let parsed = JSON.parse(jsonString, function(key, value) {
+        if (key === "accounts") {
+            this.keys = value;
+            return; // return undefined removes "accounts" from the keys
+        } else if (key === "program_id") {
+            this.programId = value;
+            return;
+        } else if (key === "is_signer") {
+            this.isSigner = value;
+            return;
+        } else if (key === "is_writable") {
+            this.isWritable = value;
+            return;
+        }
+        return value;
+    });
+    return parsed;
+}
