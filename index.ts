f062a6b5243a2625f7fe14514398acae4e6424df
PopcornPaws <lipschitz77@gmail.com>
2022-01-11 14:51:27 +0100

    Add node-fetch to index.ts.

---
+++ index.ts
@@ -0,0 +1,10 @@
+// This is required for http requests to work in Wasm
+import fetch from "node-fetch";
+// @ts-ignore
+global.fetch = fetch;
+// @ts-ignore
+global.Headers = fetch.Headers;
+// @ts-ignore
+global.Request = fetch.Request;
+// @ts-ignore
+global.Response = fetch.Response;
